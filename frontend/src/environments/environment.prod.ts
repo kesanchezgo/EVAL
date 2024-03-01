8f79f90c018928d943ad3d84d5ac3071b721bbcb
Kevin <ksanchezgo@unsa.edu.pe>
2024-03-01 12:19:34 -0500

    update

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    evalsys: 'https://vri.gestioninformacion.unsa.edu.pe/eval/'
+    evalsys: 'https://vri.gestioninformacion.unsa.edu.pe/s_eval/'
 };
